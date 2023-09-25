ac630d4decfae400dce7f4d3e7e56876d0b3fe2c
mantullz <36762688+mantullz@users.noreply.github.com>
2023-09-26 01:33:09 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -20,10 +20,6 @@ if [ "${EUID}" -ne 0 ]; then
 		echo "You need to run this script as root"
 		exit 1
 fi
-if [ "$(systemd-detect-virt)" == "openvz" ]; then
-		echo "OpenVZ is not supported"
-		exit 1
-fi
 
 localip=$(hostname -I | cut -d\  -f1)
 hst=( `hostname` )
